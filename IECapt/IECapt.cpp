c9f3690ec57e767536948e3c657f1982c7233352
Björn Höhrmann <bjoern@hoehrmann.de>
2006-11-06 16:57:30 +0100

    + updated copyright

--- IECapt/IECapt.cpp
+++ IECapt/IECapt.cpp
@@ -2,7 +2,7 @@
 //
 // IECapt - A Internet Explorer Web Page Rendering Capture Utility
 //
-// Copyright (C) 2003 Bjoern Hoehrmann <bjoern@hoehrmann.de>
+// Copyright (C) 2003-2006 Bjoern Hoehrmann <bjoern@hoehrmann.de>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
